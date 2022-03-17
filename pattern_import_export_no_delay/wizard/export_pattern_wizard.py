53164f461177fb945a702ceffb855b8bf665d206
Thomas Arbib <tarbib@wrk93.wrk.cby.camptocamp.com>
2022-03-17 13:05:55 +0100

    Fix context key

--- pattern_import_export_no_delay/wizard/export_pattern_wizard.py
+++ pattern_import_export_no_delay/wizard/export_pattern_wizard.py
@@ -4,4 +4,5 @@ class ExportPatternWizard(models.Model):
     _inherit = "export.pattern.wizard"
 
     def run(self):
-        return super().with_context(test_queue_job_no_delay=True).run()
+        self = self.with_context(test_queue_job_no_delay=True)
+        return super(ExportPatternWizard, self).run()
